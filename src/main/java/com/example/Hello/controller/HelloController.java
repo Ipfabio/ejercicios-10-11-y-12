ce9e0b7986e9ba7cf9679454e8ec8ab3a8b3101b
Fabio <fabioperrone95@hotmail.com>
2022-08-19 16:55:52 -0300

    agrego segurar y starter boostrap

--- src/main/java/com/example/Hello/controller/HelloController.java
+++ src/main/java/com/example/Hello/controller/HelloController.java
@@ -10,4 +10,22 @@ public class HelloController {
     public String holaMundo(){
         return "Hola mundo";
     }
+
+    @GetMapping("/boostrap")
+    public String boostrap(){
+        return """
+            <!doctype html>
+            <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Bootstrap demo</title>
+              </head>
+              <body>
+                <h1>Hello, world!</h1>
+              </body>
+            </html>
+            """;
+    }
 }
+
